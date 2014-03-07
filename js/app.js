28549cbd3a9bfbd8b3a8c98d28b336a5f01250f7
lathamb <lathamb@gmail.com>
2014-03-06 23:27:30 -0500

    Adding the rest of the files

---
+++ js/app.js
@@ -0,0 +1,13 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+	this.resource('about');
+	this.resource('posts');
+})
+
+var posts = [{
+	id: '1',
+	title: "Wicked"
+}]
+
+$(document).foundation();
